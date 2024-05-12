dbb8e60c0adeecbf54ea6e2819813b833bcb298b
DanteRoyal <dwroyal02@naver.com>
2024-05-12 10:48:22 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/flab/mame/MameApplication.java
@@ -0,0 +1,13 @@
+package com.flab.mame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/mame/MameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.mame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
