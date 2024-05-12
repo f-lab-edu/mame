46176c7b0788171a1bf951dd7c01f2cd8bc18aa5
DanteRoyal <dwroyal02@naver.com>
2024-05-12 10:59:10 +0900

    User 엔티티 생성

---
+++ src/main/java/com/flab/mame/domain/User.java
@@ -0,0 +1,22 @@
+package com.flab.mame.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String email;
+
+	private String password;
+
+	private String username;
+
+
+}
