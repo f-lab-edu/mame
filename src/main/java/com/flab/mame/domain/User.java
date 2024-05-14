ad96b7cb6e7bf51efec711ffb464fe01f6cdb391
DanteRoyal <dwroyal02@naver.com>
2024-05-14 10:23:20 +0900

    User 엔티티 수정

--- src/main/java/com/flab/mame/domain/User.java
+++ src/main/java/com/flab/mame/domain/User.java
@@ -1,22 +1,48 @@
 package com.flab.mame.domain;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false)
 	private String email;
 
+	@Column(nullable = false)
 	private String password;
 
-	private String username;
+	@Column(nullable = false)
+	private String name;
 
+	@Column(nullable = false)
+	private Integer age;
 
+	@Column(nullable = false)
+	private String gender;
+
+	private String bio;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<UserProfilePhoto> userProfilePhotos;
 }
